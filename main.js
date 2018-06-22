283be1683d197f581b4333e4585dd0471604f0a2
Janine Hempy <jhempy@gmail.com>
2018-06-22 10:33:09 -0400

    Plays sequence

--- main.js
+++ main.js
@@ -73,13 +73,19 @@ var simon = new Vue({
 	data: {
 		longest: 0,
 		current: 0,
-		isTimerActive: false
+		isTimerActive: false,
+		sequence: [ 'red', 'yellow', 'green', 'green', 'blue' ],
+		taps: [],
+		playSequenceId: null,
+		currentButton: '',
+		playSequenceCounter: 0
 	},
 
 	methods: {
 
 		start: function() {
 			this.isTimerActive = true;
+			this.playSequence();
 			this.$emit('stateChange', 'capturing');
 		},
 
@@ -102,6 +108,33 @@ var simon = new Vue({
 			// light up the button
 
 			// emit another event
+		},
+
+		playSequence: function() {
+
+			var self = this;
+
+			self.playSequenceId = window.setInterval(function() {
+
+				self.currentButton = self.sequence[self.playSequenceCounter];
+
+				setTimeout(function() {
+					self.currentButton = '';
+					self.playSequenceCounter++;
+					if (self.playSequenceCounter === self.sequence.length) {
+
+						// Turn off the repeat
+						console.log("Ran out of sequence to display");
+						window.clearInterval(self.playSequenceId);
+
+					}
+
+				}, 300);
+
+			}, 600);
+
+
+
 		}
 
 	}
