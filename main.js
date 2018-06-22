293bf73fa3e0209ecc98ed71c300b8d123737507
Janine Hempy <jhempy@gmail.com>
2018-06-22 09:26:12 -0400

    Adds communication between simon and timer

--- main.js
+++ main.js
@@ -1,6 +1,62 @@
 Vue.component('timer', {
 
-	template: `<p>Timer goes here!</p>`
+	template: `<div id="status">
+		<p>{{ message }}{{ remaining }}</p>
+	</div>`,
+
+	data() {
+		return {
+			message: 'Click the Start button to begin!',
+			remaining: '',
+			intervalId: null
+		}
+	},
+
+	created() {
+
+		var self = this;
+
+		this.$parent.$on('stateChange', function($event) {
+
+			switch($event) {
+			
+				case 'capturing': 
+					self.startTimer();
+					break;
+			
+				default:
+					console.log("Timer: state changed to [" + $event + "]");
+			
+			}
+			
+		});
+	},
+
+	methods: {
+
+		startTimer: function() {
+			if (this.intervalId === null) {
+				this.remaining = 10;
+				this.message = '';
+				this.intervalId = window.setInterval(this.tick, 1000);
+			}
+		},
+
+		stopTimer: function(text) {
+			window.clearInterval(this.intervalId);
+			this.message = text;
+			this.remaining = '';
+		},
+
+		tick: function() {
+			console.log('Tick!');
+			this.remaining--;
+			if (this.remaining === 0) {
+				this.stopTimer('Time expired!');
+			} 
+		}
+
+	}
 
 });
 
@@ -11,6 +67,16 @@ var simon = new Vue({
 	data: {
 		longest: 0,
 		current: 0
+	},
+
+	methods: {
+
+		start: function() {
+
+			this.$emit('stateChange', 'capturing');
+
+		}
+
 	}
 
 
